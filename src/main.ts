361b95a9541f3b1cfb9b4d93a5571e1b3dfff687
sidiik <sidiikpro@gmail.com>
2024-11-14 16:53:28 +0300

    done api changes

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.setGlobalPrefix('v1');
   app.use(cookieParser());
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://staging.edalmar.com'],
     credentials: true,
   });
 
